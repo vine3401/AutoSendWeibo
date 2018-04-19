5403464ab4d5f6e056ed27a01a3f61c07e45f7d9
李正 <liz@knownsec.com>
2018-04-19 16:06:04 +0800

    test

--- send_task.py
+++ send_task.py
@@ -18,16 +18,8 @@ class SendTask(Thread):
     def run(self):
         logger.info("start task...")
         self.sendWeibo()
-        t = time.localtime(time.time()).tm_min
-        TIME_SLOG = 60 - t
-        print("before Send:", t, TIME_SLOG)
         while not self.stopped.wait(TIME_SLOG):
-            wt = time.localtime(time.time()).tm_min
             self.sendWeibo()
-            t = time.localtime(time.time()).tm_min
-            TIME_SLOG = 60 - t
-            print("after Send:", t, TIME_SLOG)
-            print("wait seconds", abs(wt-t))
         logger.info("end task...")
 
     def stop(self):
