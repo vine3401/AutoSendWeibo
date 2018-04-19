6224914249238b353ac682fe1787a157a67a11f7
李正 <liz@knownsec.com>
2018-04-19 16:07:27 +0800

    test

--- main.py
+++ main.py
@@ -4,8 +4,8 @@ import send_task
 from weibo.weibo_login import wblogin
 from picLogin import login
 if __name__ == '__main__':
-    # (wei_session, uid) = wblogin(username, password)
-    (wei_session, uid) = login("18482065251", "Lz122521#")
+    (wei_session, uid) = wblogin("18482065251", "Lz122521#")
+    # (wei_session, uid) = login("18482065251", "Lz122521#")
     if uid is not None:
         wei_session.get('http://weibo.com/')
         task = send_task.SendTask(wei_session, uid)
