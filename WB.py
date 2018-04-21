1df126a192b32e3df176d690c63e5332f5ba9ac5
lizheng3401 <1925130654@qq.com>
2018-04-21 13:07:04 +0800

    测试

--- WB.py
+++ WB.py
@@ -7,9 +7,13 @@ from WB.wb_get_data import get_data
 from WB.wb_send import send_wb
 
 (session, uid) = login("18482065251", "Lz122521#")
+print("进程启动：")
+(text, url_pic) = get_data()
+send_wb(session, "进程重启，发送测试验证", url_pic)
 while True:
     t = time.localtime(time.time())
     if t.tm_sec == 0 and t.tm_min == 0:
+        print("发送微博Exception")
         (text, url_pic) = get_data()
         send_wb(session, text, url_pic)
     else:
